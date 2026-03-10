eda0cf0a6f46c4d23668309b30d6c2587d66c01e
abbi portwood <apwood09@gmail.com>
2026-03-09 20:55:50 -0400

    functionally to create a menu of food and drinks: burgers & milkshakes

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+// global scope: burger variable 
+const burgers = ['Hamburger', 'Cheeseburger']
+// global scope: featureDrink variable 
+let featuredDrink = 'Strawberry Milkshake'; 
+
+// function  addburger
+function addBurger() {  
+    const newBurger = 'Flatburger'; 
+    burgers.push(newBurger);
+    
+    if(true) {
+        const anotherNewBurger = 'Maple Bacon Burger';
+        burgers.push(anotherNewBurger); 
+    }
+}
+
+function changeFeaturedDrink() {
+    featuredDrink = 'The JavaShake'; 
+}
